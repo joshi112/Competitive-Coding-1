261fb629e7cf614124ca70a6317deb9aebd59560
Shreya Joshi <joshi11shreya@gmail.com>
2024-12-31 14:41:21 -0500

    Missing Number and MinHeap

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,28 @@
+public class FindMissingNumber {
+
+
+    public int findMissingNum(int[] a){
+        int low = 0;
+        int high = a.length-1;
+        int diff = a[low];
+        while(low<=high){
+            int mid = low +(high-low)/2;
+            if(a[mid] - mid == diff){
+                low = mid+1;
+            }else if(a[mid] - mid == diff+1){
+                if(a[mid] - a[mid-1] ==2 ){
+                    return a[mid]-1;
+                }
+                high = mid-1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args){
+        int[] a = {3,4,5,6,7,8};
+        FindMissingNumber fmn = new FindMissingNumber();
+        System.out.println("Missing num "+fmn.findMissingNum(a));
+    }
+}
 
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,45 @@
 
+public class MinHeap {
+    int a[];
+    public MinHeap(int[] a){
+        this.a = a;
+    }
+
+    public static void heapify(int[] a, int i, int n){
+        int smallest = i;
+        int left = 2*i +1;
+        int right = 2*i+2;
+
+        if(left < n && a[left] < a[i]){
+            smallest = left;
+        }
+        if(right < n && a[right] < a[smallest]){
+            smallest = right;
+        }
+
+        if(i != smallest){
+            int temp = a[i];
+            a[i] = a[smallest];
+            a[smallest] = temp;
+
+            heapify(a, smallest, n);
+        }
+
+    }
+
+
+    public static void main(String[] args){
+        int[] a = {5,4,3,2,1,6,7};
+        MinHeap mh = new MinHeap(a);
+        int n = a.length;
+        int nonLeafNodes = n/2-1;
+
+        for(int i=nonLeafNodes;i>=0;i--){
+            heapify(a, i, n);
+        }
+
+        for (int j : a) {
+            System.out.println(j + " ");
+        }
+    }
+}
